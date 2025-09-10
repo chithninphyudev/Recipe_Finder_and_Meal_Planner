32647e5f09875cfa59ad622f0b8aeb83ec1f9853
chithninphyudev <chithninphyudeveloper@gmail.com>
2025-09-10 15:28:25 +0630

    remove api key

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
   static const String baseUrl = 'https://api.spoonacular.com';
-  static const String apiKey = 'db3605a18bce42dfaefe0872e9ed8aa8';
+  static const String apiKey = '';
 }

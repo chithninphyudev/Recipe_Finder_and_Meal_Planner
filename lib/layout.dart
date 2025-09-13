48de07774e0a9cde0eaea3e788d680eb6a49d62b
chithninphyudev <chithninphyudeveloper@gmail.com>
2025-09-13 23:37:24 +0630

    added function to check connectivity

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 import '../features/home/views/home_page.dart';
 import 'features/favorites/views/favorite_recipes.dart';
@@ -14,12 +15,48 @@ class Layout extends ConsumerStatefulWidget {
 
 class _LayoutState extends ConsumerState<Layout> {
   int _currentPage = 0;
+  bool _isOnline = true;
 
-  final List<Widget> _pages = const [
+  final List<Widget> _onlinePages = const [
     HomePage(),
     FavoriteRecipesPage(),
     WeeklyMealPlanPage(),
   ];
+
+  final List<Widget> _offlinePages = const [
+    FavoriteRecipesPage(),
+    WeeklyMealPlanPage(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _checkInitialConnection();
+
+    Connectivity()
+        .onConnectivityChanged
+        .listen((List<ConnectivityResult> results) {
+      setState(() {
+        _isOnline = results.any((result) => result != ConnectivityResult.none);
+
+        if (!_isOnline && _currentPage == 0) {
+          _currentPage = 0;
+        }
+      });
+    });
+  }
+
+  void _checkInitialConnection() async {
+    final connectivityResult = await (Connectivity().checkConnectivity());
+    if (mounted) {
+      setState(() {
+        _isOnline = connectivityResult.any((result) => result != ConnectivityResult.none);
+        if (!_isOnline) {
+          _currentPage = 0;
+        }
+      });
+    }
+  }
 
   void _onTabTapped(int index) {
     setState(() {
@@ -29,10 +66,38 @@ class _LayoutState extends ConsumerState<Layout> {
 
   @override
   Widget build(BuildContext context) {
+    final pages = _isOnline ? _onlinePages : _offlinePages;
+
+    final navItems = _isOnline
+        ? const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Weekly Meal Plan',
+            ),
+          ]
+        : const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Weekly Meal Plan',
+            ),
+          ];
+
     return Scaffold(
       body: IndexedStack(
         index: _currentPage,
-        children: _pages,
+        children: pages,
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentPage,
@@ -40,20 +105,7 @@ class _LayoutState extends ConsumerState<Layout> {
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey,
         showUnselectedLabels: true,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favorites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'Weekly Meal Plan',
-          ),
-        ],
+        items: navItems,
       ),
     );
   }
